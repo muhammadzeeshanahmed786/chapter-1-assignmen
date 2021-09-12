c10035d47163ece4760c9809fe775bc0ea3e19f1
Muhammad Zeeshan Ahmed <78692718+shani2103792@users.noreply.github.com>
2021-09-12 04:51:52 -0700

    Add files via upload

---
+++ app.js
@@ -0,0 +1,47 @@
+
+
+// 1
+
+// alert("Hello Wolrd");
+
+
+
+// 2
+
+// alert("Please Enter a Valid Password")
+
+
+
+// 3
+
+
+// alert("Well come To JS Land...\n Happy Coding")
+
+
+
+
+// 4
+
+
+// var masg="Hello...I Can Run JS Through My Web Browsers Console."
+// console.log(masg)
+
+
+
+
+// 5
+
+
+
+// alert("HTML/CSS")
+
+
+
+
+
+
+
+
+
+
+
